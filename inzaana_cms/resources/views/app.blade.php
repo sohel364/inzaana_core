1249c3388253415a679199a6482a329aaaa999cb
Shrabon Mohsin <mohsin.ofcl@gmail.com>
2016-01-15 19:15:54 +0600

    footer updated
    Signed-off-by: Shrabon Mohsin <mohsin.ofcl@gmail.com>

--- inzaana_cms/resources/views/app.blade.php
+++ inzaana_cms/resources/views/app.blade.php
@@ -60,7 +60,7 @@
                 <div class="pull-right hidden-xs">
                     <b>Version</b> 2.0
                 </div>
-                <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+                <strong>Copyright &copy; 2016 <a href="#">Inzaana</a>.</strong> All rights reserved.
             </footer>
             
             <!-- Control Sidebar -->
--- inzaana_cms/resources/views/includes/header.blade.php
+++ inzaana_cms/resources/views/includes/header.blade.php
@@ -2,11 +2,11 @@
 <header class="main-header">
 
     <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="admin" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>A</b>LT</span>
+          <span class="logo-mini"><b>I</b>cms</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Admin</b>LTE</span>
+          <span class="logo-lg"><b>Inzaana</b>CMS</span>
         </a>
 
 
