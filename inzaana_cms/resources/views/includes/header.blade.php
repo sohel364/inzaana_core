da871f0e0c92dd48636005a3bd78f35a8747f467
Tajuddin Khandaker <tajuddin.khandaker.cse.ju@gmail.com>
2016-01-17 21:59:23 +0900

    [views][sign out routing is done]

--- inzaana_cms/resources/views/includes/header.blade.php
+++ inzaana_cms/resources/views/includes/header.blade.php
@@ -239,7 +239,7 @@
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="/sign-in" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
