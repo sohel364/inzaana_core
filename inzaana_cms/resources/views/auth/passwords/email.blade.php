edc0bbd72355aedbdec32045e026a3680cae5736
Shrabon Mohsin <mohsin.ofcl@gmail.com>
2016-01-31 23:48:14 +0600

    Reset password page design error solved.
    Signed-off-by: Shrabon Mohsin <mohsin.ofcl@gmail.com>

--- inzaana_cms/resources/views/auth/passwords/email.blade.php
+++ inzaana_cms/resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,54 @@
-@extends('layouts.master')
+@extends('layouts.master_out')
+@section('title', 'Reset passwors')
+@section('header-style')
+ <link href="{{ URL::asset('css/email.css') }}" rel="stylesheet" type="text/css">  
+@endsection
 
 <!-- Main Content -->
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+        <div class="col-md-6 col-md-offset-3">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
+        <div class="panel panel-default padPtop">
+          <div class="panel-heading text-center panHead">
+           <h2 class="CustomHead" ><i class="fa fa-key"></i> Reset Your Password</h2>
+          </div>
+          <div class="panel-body pad50">
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+            
+            <form class="emailPad" role="form" method="POST" action="{{ url('/password/email') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                            <div class="col-md-12">
+                                <input type="email" placeholder="Your Email Address" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
+                                <!--@if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                @endif-->
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
+                        <div class="form-group padtop">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-info">
+                                    <i class="fa fa-btn fa-envelope"></i> Reset
                                 </button>
+                                <a href="{{ url('/') }}" class="btn btn btn-info"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a>
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
+          </div>
+        </div>
         </div>
     </div>
 </div>
