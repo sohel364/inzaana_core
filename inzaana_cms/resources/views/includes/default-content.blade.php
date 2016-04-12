ccf46ea5a919bde1f3a106306cc65f0ff8d76d07
Jisan Mahmud <jisanmahmud@yahoo.com>
2016-04-13 00:13:52 +0600

    default menu bg bug fixed

--- inzaana_cms/resources/views/includes/default-content.blade.php
+++ inzaana_cms/resources/views/includes/default-content.blade.php
@@ -1,7 +1,7 @@
 <?php //include ("templates/$category_name/$template_name/body.html");?>
 
-<div id="container_background-1" style="width: 100%; height: 900px;">
-       <div id="container_background-1_div-1">
+<div id="container_background-1" style="width: 100%; height: 1200px;">
+       <div id="container_background-1_div-1" style="width: 100%; height: 1200px;">
             <div id="hint" name="textarea" style="position: absolute; width: 70%; height: 300px; border: 2px lightgray dotted; left: 15%; top: 30%;">
                 <h1><span style="position: absolute; color: lightgray; left: 33%; top: 35%;">{{ $currentMenu . ': Drop Items Here' }} </span></h1>
             </div>
