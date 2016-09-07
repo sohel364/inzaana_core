93a4fd4b59ccc46a6105aa185d59326c9abbd51c
Tajuddin Khandaker <tajuddin.khandaker.cse.ju@gmail.com>
2016-09-08 05:08:30 +0900

    [view][Store count at sidebar]

--- inzaana_cms/resources/views/includes/sidebar.blade.php
+++ inzaana_cms/resources/views/includes/sidebar.blade.php
@@ -33,7 +33,7 @@
 			
 			<li class="treeview">
               <a href="#">
-                <i class="fa fa-globe"></i> <span>Store</span> 
+                <i class="fa fa-globe"></i> <span>Store <small class="label bg-green">{{ Inzaana\Store::count() }}</small></span> 
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
