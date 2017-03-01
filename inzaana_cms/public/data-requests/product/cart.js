c849717d833e665363e73ea445d8f58d24cb55e8
asadku34 <asadku34@gmail.com>
2017-03-01 21:21:01 +0600

    [Add to cart completed]

--- inzaana_cms/public/data-requests/product/cart.js
+++ inzaana_cms/public/data-requests/product/cart.js
@@ -1,14 +1,15 @@
 jQuery(document).on('click','#add_to_cart',function(){
     var data = {
         cart_item: {
-            product_id:    $(this).data('pid'),
+            product_id: $(this).data('pid'),
             title:      $(this).siblings('div').find('#p-title').text(),
             price:      $(this).siblings('div').find('#p-price').text(),
             image_url:  $(this).siblings('img').attr('src')
         }
     };
     $(this).text("Added");
-    $(this).removeAttr('id');
+    //$(this).removeAttr('id');
+    $(this).attr('id','add_to_cart-inactive');
     $.ajax({
         async: true,
         type: 'GET',
@@ -30,6 +31,7 @@ jQuery(document).on('click','#add_to_cart',function(){
 jQuery(document).on('click','#cart-item-remove-btn',function(e){
     e.preventDefault();
     var define_this = this;
+    var pid = $(this).data('pid');
     $.ajax({
         async: true,
         type: 'GET',
@@ -39,6 +41,7 @@ jQuery(document).on('click','#cart-item-remove-btn',function(e){
             if(data){
                 $('#cart_items_count').text($('#cart_items_count').text()-1);
                 $(define_this).closest('li').remove();
+                $('a[data-pid="'+pid+'"]').attr('id','add_to_cart').text("Add to Cart");
             }
         },
         error: function(data){
